cfbfdb3b2f8209b4b3c220640dffec47834babd4
Caden Jurkovski <jurko036@umn.edu>
2024-10-19 00:24:26 -0500

    added some basic post request functions and basic html/js for the url shortener

--- app/routes/url-shortener.js
+++ app/routes/url-shortener.js
@@ -1,11 +1,26 @@
 import express from "express";
+import axios from "axios";
 
 const app = express.Router();
 
 app.use(express.static("public")); // allows public access for css, assets, etc
+app.use(express.json()); // parses json bodies
 
 app.get("/", (req, res) => {
     res.render("url-shortener.ejs");
+});
+
+// post endpoint to shorten urls
+app.post("/shorten", async (req, res) => {
+    const { url } = req.body;
+    try {
+        const response = await axios.post("https://short.jurkovski.com/shorten", { url }, {
+            headers: { "Content-Type": "application/json" }
+        });
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to shorten URL" });
+    }
 });
 
 export default app;
