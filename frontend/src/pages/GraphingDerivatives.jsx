25ee9ba1e55792134f4695e18643f6238e4b4a96
Caden <84691900+cyphercrit@users.noreply.github.com>
2024-11-02 21:05:53 -0500

    Worked on fix, still need to fix mathquill and desmos imports

--- frontend/src/pages/GraphingDerivatives.jsx
+++ frontend/src/pages/GraphingDerivatives.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import styles from './GraphingDerivatives.module.css';
-import Header from '../components/Header.js';
-import Footer from '../components/Footer.js';
+import Header from '../components/Header.jsx';
+import Footer from '../components/Footer.jsx';
 import Desmos from 'desmos';
 import MathQuill from 'mathquill';
 
